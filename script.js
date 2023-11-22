645b8d6f0abbaec623930d4b466e6369d8aaa2f6
RaphaelPesce <145458696+RaphaelPesce@users.noreply.github.com>
2023-11-22 08:05:33 +0100

    original5

--- script.js
+++ script.js
@@ -15,29 +15,31 @@ const audioList = [
 
 // Récupération des éléments du DOM
 const audioPlayer = document.getElementById('audioPlayer');
-const preCountdownTimer = document.getElementById('preCountdownTimer'); // Élément pour le décompte avant la lecture
-const countdownTimer = document.getElementById('countdownTimer');
-const timerElement = document.getElementById('timer');
-const messageElement = document.getElementById('endMessage'); // Élément pour le message de fin
+const preCountdownTimer = document.getElementById('preCountdownTimer'); // Pour le décompte avant la lecture
+const countdownTimer = document.getElementById('countdownTimer'); // Pour le décompte après la lecture
+const timerElement = document.getElementById('timer'); // Pour afficher le temps restant
+const messageElement = document.getElementById('endMessage'); // Pour afficher le message de fin
+const playButton = document.getElementById('playButton'); // Le bouton pour démarrer la lecture
 
 // Ajout d'un écouteur d'événement sur le bouton pour démarrer la lecture
-document.getElementById('playButton').addEventListener('click', function () {
-    startPreCountdown(3); // Décompte de 5 secondes avant de jouer le son
+playButton.addEventListener('click', function () {
+    startPreCountdown(2); // Lancer le décompte de 5 secondes avant de jouer le son
+    this.disabled = true; // Désactiver le bouton pour empêcher des clics supplémentaires
 });
 
 // Fonction pour démarrer un décompte avant de jouer le son
 function startPreCountdown(duration) {
     let time = duration;
-    preCountdownTimer.textContent = `Time : ${time} secondes`;
-    preCountdownTimer.style.display = 'block';
+    preCountdownTimer.textContent = `Le son va commencer dans : ${time} secondes`;
+    preCountdownTimer.style.display = 'block'; // Afficher le timer
 
     const interval = setInterval(function () {
         time--;
-        preCountdownTimer.textContent = `time : ${time} secondes`;
+        preCountdownTimer.textContent = `Le son va commencer dans : ${time} secondes`;
 
         if (time <= 0) {
             clearInterval(interval);
-            preCountdownTimer.style.display = 'none';
+            preCountdownTimer.style.display = 'none'; // Cacher le timer
             playRandomSound(); // Jouer le son après le décompte
         }
     }, 1000);
@@ -45,36 +47,39 @@ function startPreCountdown(duration) {
 
 // Fonction pour jouer un son aléatoire de la liste
 function playRandomSound() {
-    const randomIndex = Math.floor(Math.random() * audioList.length);
-    const selectedSound = audioList[randomIndex];
+    const randomIndex = Math.floor(Math.random() * audioList.length); // Choisir un index aléatoire
+    const selectedSound = audioList[randomIndex]; // Récupérer le son sélectionné
 
-    audioPlayer.src = selectedSound.src;
-    audioPlayer.play();
+    audioPlayer.src = selectedSound.src; // Définir la source du lecteur audio
+    audioPlayer.play(); // Jouer le son
 
+    // Lorsque le son se termine, lancer le décompte de fin
     audioPlayer.onended = function () {
-        startPostCountdown(5, selectedSound.message);
+        startPostCountdown(5, selectedSound.message); // Lancer le décompte après la fin du son
     };
 }
 
+// Fonction pour démarrer un décompte après la fin de la lecture
 function startPostCountdown(duration, message) {
     let time = duration;
-    countdownTimer.style.display = 'block';
-    timerElement.textContent = time;
-    messageElement.style.display = 'none';
+    countdownTimer.style.display = 'block'; // Afficher le timer
+    timerElement.textContent = time; // Mettre à jour le texte du timer
+    messageElement.style.display = 'none'; // Cacher le message initial
 
     const interval = setInterval(function () {
         time--;
-        timerElement.textContent = time;
+        timerElement.textContent = time; // Mettre à jour le temps restant
 
         if (time <= 0) {
             clearInterval(interval);
-            countdownTimer.style.display = 'none';
-            messageElement.textContent = message;
-            messageElement.style.display = 'block';
+            countdownTimer.style.display = 'none'; // Cacher le timer
+            messageElement.textContent = message; // Afficher le message de fin
+            messageElement.style.display = 'block'; // Rendre le message visible
 
-            // Définir un délai pour cacher le message après 5 secondes
+            // Réactiver le bouton après que le message a été caché
             setTimeout(function () {
-                messageElement.style.display = 'none';
+                messageElement.style.display = 'none'; // Cacher le message
+                playButton.disabled = false; // Réactiver le bouton
             }, 5000);
         }
     }, 1000);
